d5e0d6e5c83553232b5663986bfdeafc4ed6b4f3
Simon Adamov <simon.adamov@mailbox.org>
2023-06-15 15:00:16 +0200

    formatted and linted

--- src/weathergraphnet/create_gif.py
+++ src/weathergraphnet/create_gif.py
@@ -1,13 +1,31 @@
-import matplotlib.animation as animation #type: ignore
-import matplotlib.pyplot as plt #type: ignore
+"""Create an animated gif of a variable from a NetCDF file using matplotlib.
+
+The module imports the necessary libraries, defines the path to the NetCDF file, and
+opens the file using xarray. It then defines the variable to plot, calculates the
+minimum and maximum values of the variable, and creates a new figure object. The first
+time step of the variable is plotted, and an update function is defined to update the
+plot for each frame of the animation. Finally, the animation is saved as a gif with the
+specified filename.
+
+Example usage:
+    $ python create_gif.py
+
+Output:
+    A gif animation of the specified variable from the NetCDF file.
+"""
+# Third-party
+import matplotlib.pyplot as plt  # type: ignore
 import xarray as xr
+from matplotlib import animation  # type: ignore
 
 # Define the path to the NetCDF file
-nc_file='prog_vars_-30.0_4000.0_1000.0_DOM01_ML_-30.0_4000.0_1000.0_20080801T000000Z.nc'
-nc_path='/scratch/sadamov/icon/icon-nwp/cpu/experiments/atmcirc-straka_93/' + nc_file
+nc_file = (
+    "prog_vars_-30.0_4000.0_1000.0_DOM01_ML_-30.0_4000.0_1000.0_20080801T000000Z.nc"
+)
+nc_path = "/scratch/sadamov/icon/icon-nwp/cpu/experiments/atmcirc-straka_93/" + nc_file
 
 # Define the variable to plot
-var_name = 'temp'
+var_name = "temp"
 
 # Open the NetCDF file
 ds = xr.open_dataset(nc_path)
@@ -23,26 +41,23 @@ fig, ax = plt.subplots()
 # Plot the first time step of the variable
 im = var.isel(time=0).plot(vmin=var_min, vmax=var_max, ax=ax)
 plt.gca().invert_yaxis()  # type: ignore # invert the y-axis
-plt.title('Time: 0 seconds')
+plt.title("Time: 0 seconds")
+
 
 def update(frame):
     # Update the data of the current plot
     time_in_seconds = round((var.time[frame] - var.time[0]).item() * 24 * 3600)
     im.set_array(var.isel(time=frame))
-    plt.title(f'Time: {time_in_seconds:.0f} seconds')
+    plt.title(f"Time: {time_in_seconds:.0f} seconds")
     return im
 
+
 ani = animation.FuncAnimation(
-    fig, 
-    update, 
-    frames=var.shape[0],
-    interval=100, 
-    blit=False
+    fig, update, frames=var.shape[0], interval=100, blit=False
 )
 
 # Define the filename for the output gif
-output_filename = 'animation.gif' + nc_file
+output_filename = "animation.gif" + nc_file
 
 # Save the animation as a gif
-ani.save(output_filename, writer='imagemagick', dpi=100)
-
+ani.save(output_filename, writer="imagemagick", dpi=100)
--- src/weathergraphnet/dataloader.py
+++ src/weathergraphnet/dataloader.py
@@ -1,32 +1,39 @@
+"""Functionality for loading weather data from NetCDF files.
+
+The data is assumed to be in a specific directory structure and file naming convention,
+which is checked using regular expressions. The loaded data is chunked along the
+"member" and "time" dimensions for efficient storage in the Zarr archive. If the Zarr
+archive already exists, new data is appended to it. Otherwise, a new Zarr archive is
+created.
+"""
+# Import the necessary libraries
+# Standard library
 import os
 import re
 
+# Third-party
 import xarray as xr
-import zarr # type: ignore
 
-# Define the path to the data
+# Define the path to the directory containing the data files
 data_path = "/scratch/sadamov/icon/icon-nwp/cpu/experiments/"
 
-# Define the regular expression to match the filename
+# Define a regular expression to match the filename of the data files
 filename_regex = r"atmcirc-straka_93_(.*)DOM01_ML_20080801T000000Z.nc"
 
-# Compile the regular expression
+# Compile the regular expression into a pattern object
 filename_pattern = re.compile(filename_regex)
 
-# Define the path to the Zarr archive
-zarr_path = "/scratch/sadamov/icon/icon-nwp/cpu/experiments/data.zarr"
+# Define the path to the Zarr archive where the data will be stored
+zarr_path = "/scratch/sadamov/icon/icon-nwp/cpu/experiments/data_combined.zarr"
 
-# Define the encoding for the Zarr archive
-encoding = {"member": {"chunks": 1}, "time": {"chunks": "auto"}}
-
-# Get a list of all the folders in the data path
+# Get a list of all the folders in the data directory
 folders = os.listdir(data_path)
 
-# Loop over each folder and load the data
+# Loop over each folder and load the data files
 for folder in folders:
-    # Check if the folder matches the naming convention
+    # Check if the folder matches the naming convention for the data files
     if folder.startswith("atmcirc-straka_93_"):
-        # Define the path to the data file
+        # Define the path to the folder containing the data files
         file_path = os.path.join(data_path, folder)
         # Get a list of all the files in the folder
         files = os.listdir(file_path)
@@ -35,42 +42,33 @@ for folder in folders:
             match = filename_pattern.match(file)
             if match:
                 # Load the data using xarray
-                data = xr.open_dataset(os.path.join(file_path, file))
+                data = xr.open_dataset(os.path.join(file_path, file), engine="netcdf4")
                 # Add a new dimension called "member" and set its values based on the
                 # matched string
-                data.coords["member"] = match.group(1)
-                # Store the data in the Zarr archive with chunking along the "member"
-                # and "time" dimensions
-                data.to_zarr(
-                    zarr_path,
-                    mode="a",
-                    group=folder,
-                    compute=False,
-                    encoding=encoding
+                data.coords["member"] = xr.DataArray([match.group(1)], dims=["member"])
+                # Make "member" a dimension as well as a coordinate
+                data = data.set_index(member="member")
+                # Chunk the data along the "member" and "time" dimensions for efficient
+                # storage
+                data = data.chunk(
+                    chunks={
+                        "member": 1,
+                        "time": -1,
+                        "height": -1,
+                        "height_2": -1,
+                        "height_3": -1,
+                    }
                 )
-
-# Combine the data from the Zarr archive into a single xarray dataset
-# Open the Zarr archive
-zarr_file = zarr.open(zarr_path, mode="r")
-
-# Initialize an empty list to hold the arrays
-arrays = []
-
-# Iterate over all the groups in the archive
-for group in zarr.hierarchy.walk_groups(zarr_file):
-    # Access the data in the group
-    data = group["my_array"]
-    # Append the array to the list
-    arrays.append(data)
-
-# Concatenate the arrays along the first axis
-combined_data = zarr.concatenate(arrays, axis=0)
-
-# Compute the combined dataset and store it in the Zarr archive with chunking along the
-# "member" and "time" dimensions
-combined_data.to_zarr(
-    zarr_path.replace("data.zarr", "data_combined.zarr"),
-    mode="a",
-    compute=True,
-    encoding=encoding
-)
+                # Check if the Zarr archive already exists
+                if os.path.exists(zarr_path):
+                    # If the Zarr archive exists, open it in "a" (append) mode
+                    data.to_zarr(
+                        zarr_path,
+                        mode="a",
+                        compute=True,
+                        consolidated=True,
+                        append_dim="member",
+                    )
+                else:
+                    # If the Zarr archive does not exist, create it in "w" (write) mode
+                    data.to_zarr(zarr_path, mode="w", compute=True, consolidated=True)
