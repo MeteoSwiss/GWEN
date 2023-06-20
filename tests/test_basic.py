be0dc7caca156d72cfcda914a1dd254d1f418412
Simon Adamov <simon.adamov@mailbox.org>
2023-06-20 23:07:06 +0200

    simple debug test

---
+++ tests/test_basic.py
@@ -0,0 +1,14 @@
+def test_addition():
+    assert 1 + 1 == 2
+
+
+def test_subtraction():
+    assert 5 - 3 == 2
+
+
+def test_multiplication():
+    assert 2 * 3 == 6
+
+
+def test_division():
+    assert 10 / 2 == 5
