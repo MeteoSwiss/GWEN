b93979016147f634e2ac60be956b7aa10de95bfa
Simon Adamov <simon.adamov@mailbox.org>
2023-06-21 00:57:24 +0200

    missed dependency

--- tests/test_weathergraphnet/test_train_gnn.py
+++ tests/test_weathergraphnet/test_train_gnn.py
@@ -21,14 +21,25 @@ def test_gnn_model():
         None
 
     """
+
+    # Define the input and output channel sizes
     in_channels = 10
     out_channels = 10
+
+    # Create an instance of the GNNModel class
     model = GNNModel(in_channels, out_channels)
+
+    # Create some dummy input data
     data = Data(
         x=torch.randn((in_channels, 1)),  # pylint: disable=no-member
         edge_index=torch.tensor(  # pylint: disable=no-member
-            [[0, 1]], dtype=torch.long  # pylint: disable=no-member
+            [[0, 1], [1, 2], [2, 3], [3, 4], [4, 5], [5, 6], [6, 7], [7, 8], [8, 9]],
+            dtype=torch.long,  # pylint: disable=no-member
         ),
     )
+
+    # Pass the input data through the model
     output = model(data)
+
+    # Check that the output has the expected shape
     assert output.shape == (out_channels, 1)
